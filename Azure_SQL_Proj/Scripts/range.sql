4fa303c4d80b93161fe80eb93b9a82229841f3be
appoloniusrussels <bikitamuli.bt@gmail.com>
2023-05-01 12:57:02 +0530

    commit

--- Azure_SQL_Proj/Scripts/range.sql
+++ Azure_SQL_Proj/Scripts/range.sql
@@ -12,14 +12,14 @@ r_cte as
     select c.trade_id, c.trade_timestamp, c.trade_stock, c.quantity, c.Price, c.rnum
     ,case when DATEDIFF(SECOND, r.Trade_Timestamp, c.Trade_Timestamp) <=10 then 0 else flg+1 end flg
     , cast(abs(r.price-c.Price)/r.Price *100 as decimal) as price_diff
-    , cast(r.TRADE_ID+','+c.TRADE_ID as varchar) as lineage 
-    , convert(varchar,concat(r.Price,',',c.price) ) as trade_price
+    , cast(r.TRADE_ID+'.'+c.TRADE_ID as varchar) as lineage 
+    , convert(varchar,concat(r.Price,'.',c.price) ) as trade_price
     from cte1 c inner join r_cte r on c.trade_stock=r.trade_stock 
     and c.rnum>r.rnum
 
 )
-select Trade1 = PARSENAME(REPLACE(lineage, ',', '.'), 2) 
-,Trade2 = PARSENAME(REPLACE(lineage, ',', '.'), 1) 
+select Trade1 = PARSENAME(lineage, 2) 
+,Trade2 = PARSENAME(lineage, 1) 
 ,Trade1_price = PARSENAME(REPLACE(trade_price, ',', '.'), 2) 
 ,Trade2_price = PARSENAME(REPLACE(trade_price, ',', '.'), 1) 
 , price_diff from r_cte where flg=0
